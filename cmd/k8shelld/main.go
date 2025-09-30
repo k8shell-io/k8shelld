54af90fbb3cd160b721233346b4ec7a42bf9bf87
bruckins <karelnebojsa@seznam.cz>
2025-09-30 20:41:29 +0200

    docker sock symlink

--- cmd/k8shelld/main.go
+++ cmd/k8shelld/main.go
@@ -54,13 +54,11 @@ func main() {
 
 	dockerSocketPath := "/var/run/docker/docker.sock"
 	dockerSocketSymlink := "/var/run/docker.sock"
-	if _, err := os.Stat(dockerSocketPath); err == nil {
-		if _, err := os.Lstat(dockerSocketSymlink); err != nil {
-			if err := os.Symlink(dockerSocketPath, dockerSocketSymlink); err != nil {
-				logger.Error().Msgf("Error creating docker socket symlink: %v", err)
-			} else {
-				logger.Info().Msgf("Created Docker socket symlink: %s -> %s", dockerSocketSymlink, dockerSocketPath)
-			}
+	if _, err := os.Lstat(dockerSocketSymlink); err != nil {
+		if err := os.Symlink(dockerSocketPath, dockerSocketSymlink); err != nil {
+			logger.Error().Msgf("Error creating docker socket symlink: %v", err)
+		} else {
+			logger.Info().Msgf("Created Docker socket symlink: %s -> %s", dockerSocketSymlink, dockerSocketPath)
 		}
 	}
 
